0421287053bca323970706a1f2718532fbf0b7fc
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2023-04-28 23:51:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ rhino3dm().then(async m => {
     //RhinoCompute.url = getAuth( 'RHINO_COMPUTE_URL' ) // RhinoCompute server url. Use http://localhost:8081 if debugging locally.
     //RhinoCompute.apiKey = getAuth( 'RHINO_COMPUTE_KEY' )  // RhinoCompute server api key. Leave blank if debugging locally.
     
-    RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
+    //RhinoCompute.url = 'http://localhost:8081/' //if debugging locally.
 
 
     // load a grasshopper file!
