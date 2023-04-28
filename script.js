422a97d595e8a2686fc663de43d29fe3d2a0e496
Etterlivet <78481603+Etterlivet@users.noreply.github.com>
2023-04-28 23:44:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Import libraries
-import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { Rhino3dmLoader } from 'three/addons/loaders/3DMLoader.js'
-import rhino3dm from 'rhino3dm'
+ 
 import { RhinoCompute } from 'rhinocompute'
+
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js'
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
+import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
+import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
 
 const definitionName = 'Assignment_ziqi cui.gh'
 
